efe2f164843cc7b4d8183d278f868e23809d714e
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-22 14:41:11 +0000

    Add FollowerSerializer and create function to handle duplication errors

--- followers/models.py
+++ followers/models.py
@@ -24,8 +24,10 @@ class Follower(models.Model):
         User, related_name='followed', on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         ordering = ['-created_at']
         unique_together = ['owner', 'followed']
+        
     def __str__(self):
         return f'{self.owner} {self.followed}'

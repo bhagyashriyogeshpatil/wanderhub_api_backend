89fcc211ef8ede5b77c2b260d1c73bca4a8ba6a1
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-22 17:57:26 +0000

    Refactor Profile list and detail views to use generic views

--- profiles/views.py
+++ profiles/views.py
@@ -1,71 +1,24 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from wanderhub_api_backend.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from wanderhub_api_backend.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
     Returns a list of all profiles.
-
-    Fetches all Profile objects from the database, serializes them, 
-    and returns the data in a JSON response.
+    No create view as profile creation is handled by django signals.
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
-    """
-    Handles retrieving and updating a profile by its ID.
-
-    Methods:
-    --------
-    - get_object(pk): 
-        Retrieves a profile by ID or raises a 404 error.
-
-    - get(request, pk): 
-        Returns the profile data for the given ID.
-
-    - put(request, pk): 
-        Updates the profile with the given ID. Returns updated data or a 400 error.
-    """
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self,pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile,
-            context={'request': request}
-        )
-        return Response(serializer.data)
-        
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile,
-            data=request.data,
-            context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
--- savedposts/views.py
+++ savedposts/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics, permissions
 from wanderhub_api_backend.permissions import IsOwnerOrReadOnly
 from .models import SavedPost

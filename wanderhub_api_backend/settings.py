6be451c11f7e2e44b14d7054975d050ba9015cc8
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-24 17:45:23 +0000

    Update ALLOWED_HOSTS and CLIENT_ORIGIN_DEV for frontend development

--- wanderhub_api_backend/settings.py
+++ wanderhub_api_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -62,8 +63,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-bhagyashriy-wanderhubap-zda90yavtn9.ws.codeinstitute-ide.net',
-                'localhost', 'wanderhub-api-backend-8af792a9ebf9.herokuapp.com']
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost', 
+    '8000-bhagyashriy-wanderhubap-zda90yavtn9.ws.codeinstitute-ide.net',
+    ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-bhagyashriy-wanderhubap-zda90yavtn9.ws.codeinstitute-ide.net',
@@ -117,9 +121,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
